070e2310cc33d456d6ef786f5b93f05bc4683e75
hj <hyjoon333@gmail.com>
2026-01-27 16:23:45 +0900

    after install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 cd /home/ec2-user/backend
 
-# 기존 node_modules 삭제 (충돌 방지) 및 재설치
-rm -rf node_modules
+# nvm 환경 변수 로드 (Node/npm 위치를 알려줍니다)
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# 만약 위 코드로도 npm을 못 찾으면 직접 경로를 추가 (Amazon Linux 기준)
+export PATH=$PATH:/home/ec2-user/.nvm/versions/node/$(nvm current)/bin
+
 npm install
